aa564b743ab21eb4a7f65947e192678f18e790cc
Samuel Fernández <samueve24@gmail.com>
2026-04-06 01:44:18 -0300

    Add files via upload

---
+++ Proyecto Nuevo/database.py
@@ -0,0 +1,22 @@
+INSCRIPCIONES = []
+
+CURSOS = {
+    "word": {"titulo": "Microsoft Word", "codigo": "ADM-01", "cat": "ADM", "inscriptos": 0},
+    "excel_bas": {"titulo": "Excel Inicial", "codigo": "ADM-02", "cat": "ADM", "inscriptos": 0},
+    "excel_av": {"titulo": "Excel Avanzado", "codigo": "ADM-03", "cat": "ADM", "inscriptos": 50},
+    "powerpoint": {"titulo": "PowerPoint", "codigo": "ADM-04", "cat": "ADM", "inscriptos": 0},
+    "access": {"titulo": "Microsoft Access", "codigo": "ADM-05", "cat": "ADM", "inscriptos": 0},
+    "ingles": {"titulo": "Inglés Técnico", "codigo": "IDM-01", "cat": "IDM", "inscriptos": 0},
+    "portugues": {"titulo": "Portugués Profesional", "codigo": "IDM-02", "cat": "IDM", "inscriptos": 0},
+    "aleman": {"titulo": "Alemán Inicial", "codigo": "IDM-03", "cat": "IDM", "inscriptos": 0},
+    "espanol": {"titulo": "Español Académico", "codigo": "IDM-04", "cat": "IDM", "inscriptos": 0},
+    "italiano": {"titulo": "Italiano Conversacional", "codigo": "IDM-05", "cat": "IDM", "inscriptos": 0},
+    "frances": {"titulo": "Francés Básico", "codigo": "IDM-06", "cat": "IDM", "inscriptos": 0},
+    "python_20": {"titulo": "Python en 20 días", "codigo": "PROG-01", "cat": "PROG", "inscriptos": 0},
+    "web_6m": {"titulo": "Desarrollo Web en 6 meses", "codigo": "PROG-02", "cat": "PROG", "inscriptos": 0},
+    "java_6m": {"titulo": "Java en 6 meses", "codigo": "PROG-03", "cat": "PROG", "inscriptos": 0},
+    "refrigeracion": {"titulo": "Refrigeración", "codigo": "TEC-01", "cat": "TEC", "inscriptos": 0},
+    "plomeria": {"titulo": "Plomería", "codigo": "TEC-02", "cat": "TEC", "inscriptos": 0},
+    "electricidad": {"titulo": "Electricidad", "codigo": "TEC-03", "cat": "TEC", "inscriptos": 0},
+    "mecanica": {"titulo": "Mecánica", "codigo": "TEC-04", "cat": "TEC", "inscriptos": 0},
+}
